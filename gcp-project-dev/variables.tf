b23760d6cb67438405518df0ea6a4ebb932ebdb9
Sohan Patil <sohan.patil98@gmail.com>
2025-02-10 02:17:35 -0500

    added dev env files

---
+++ gcp-project-dev/variables.tf
@@ -0,0 +1,45 @@
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+variable "project_id" {
+  type        = string
+  description = "Project ID"
+}
+variable "region" {
+  type        = string
+  description = "region"
+}
+variable "machine_type" {
+  type        = string
+  description = "Machine Type for the cluster"
+}
+variable "disk_type" {
+  type        = string
+  description = "Disk Type for the cluster"
+}
+variable "pod_ranges" {
+  type        = string
+  description = "Pod Ranges IP Allocation"
+}
+variable "service_ranges" {
+  type        = string
+  description = "Service Ranges IP Allocation"
+}
+variable "bastion_machine_type" {
+  type        = string
+  description = "The machine type for the bastion host"
+}
+variable "bastion_disk_size" {
+  type        = number
+  description = "Boot disk size in GB"
+}
+variable "bastion_disk_type" {
+  type        = string
+  description = "Boot disk type"
+}
+variable "startup_script_path" {
+  type        = string
+  description = "Path to the bastion host startup script"
+  default     = "../modules/bastion/scripts/startup_script.sh"
+}
