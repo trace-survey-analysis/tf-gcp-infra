feb67f7141b9169b1b829423f21a5d4e53251baf
Sohan Patil <sohan.patil98@gmail.com>
2025-04-04 14:15:01 -0400

    feat: added apis

--- gcp-org/project-demo.tf
+++ gcp-org/project-demo.tf
@@ -28,3 +28,33 @@ resource "google_project_service" "secretmanager_demo" {
   service            = "secretmanager.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "logging_demo" {
+  project            = var.demo_project_id
+  service            = "logging.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "monitoring_demo" {
+  project            = var.demo_project_id
+  service            = "monitoring.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloudresourcemanager_demo" {
+  project            = var.demo_project_id
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "stackdriver_demo" {
+  project            = var.demo_project_id
+  service            = "stackdriver.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam_demo" {
+  project            = var.demo_project_id
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
+}
--- gcp-org/project-dev.tf
+++ gcp-org/project-dev.tf
@@ -29,3 +29,33 @@ resource "google_project_service" "secretmanager_dev" {
   service            = "secretmanager.googleapis.com"
   disable_on_destroy = false
 }
+# a08
+resource "google_project_service" "logging_dev" {
+  project            = var.dev_project_id
+  service            = "logging.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "monitoring_dev" {
+  project            = var.dev_project_id
+  service            = "monitoring.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloudresourcemanager_dev" {
+  project            = var.dev_project_id
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "stackdriver_dev" {
+  project            = var.dev_project_id
+  service            = "stackdriver.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam_dev" {
+  project            = var.dev_project_id
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
+}
